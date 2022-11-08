19247407bfae5d9e4b090ef3c4dce2e575e95188
Powlinett <pauline.eustachy@gmail.com>
2022-11-08 17:50:06 +0100

    wagon race solution

--- 02-dom-and-events/livecode/lib/wagon_race.js
+++ 02-dom-and-events/livecode/lib/wagon_race.js
@@ -1,1 +1,31 @@
 // TODO: write your code here
+// ajouter un event listener qui va suivre un keyup
+// savoir quelle touche a été appuyée
+// si c'est "a" voitureA avance
+// si c'est "b" voitureB avance
+// avancer = retirer la class active et l'ajouter au suivant
+
+const isPartyFinished = () => {
+  const lastTd = document.querySelector('.active + .finish');
+  return lastTd;
+};
+
+const startCar = (id) => {
+  const player = document.querySelector(id);
+    const tdActive = player.querySelector('.active');
+    const nextTd = player.querySelector('.active + td');
+    tdActive.classList.remove('active');
+    nextTd.classList.add('active');
+};
+
+document.addEventListener('keyup', (event) => {
+  if (!isPartyFinished()) {
+    const key = event.key;
+    if (key === "a") {
+      startCar("#player1-race");
+    } else if (key === "b") {
+      startCar("#player2-race");
+    }
+  }
+});
+
